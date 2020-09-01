7fc3e577fd79ab0ef42fb60f6ccefcb0fc18355b
TXG-Ryan <ryan.brown@txg.ie>
2020-09-01 22:49:25 +0930

    code change

--- Eth-Chart.js
+++ Eth-Chart.js
@@ -1,6 +1,6 @@
 const ethereumData = async () => {
 	const response = await fetch(
-		"https://min-api.cryptocompare.com/data/v2/histoday?fsym=ETH&tsym=USD&limit=119&api_key=0646cc7b8a4d4b54926c74e0b20253b57fd4ee406df79b3d57d5439874960146"
+		"https://min-api.cryptocompare.com/data/v2/histohour?fsym=ETH&tsym=USD&limit=119&api_key=0646cc7b8a4d4b54926c74e0b20253b57fd4ee406df79b3d57d5439874960146"
 	);
 	const json = await response.json();
 	const data = json.Data.Data;
