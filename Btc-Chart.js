f36f20abc97daa9b0cf649b418461d635312d343
TXG-Ryan <ryan.brown@txg.ie>
2020-09-01 22:34:36 +0930

    minute to day chart

--- Btc-Chart.js
+++ Btc-Chart.js
@@ -1,7 +1,7 @@
 ///  Calling API and modeling data for each chart ///
 const btcData = async () => {
 	const response = await fetch(
-		"https://min-api.cryptocompare.com/data/v2/histominute?fsym=BTC&tsym=USD&limit=119&api_key=0646cc7b8a4d4b54926c74e0b20253b57fd4ee406df79b3d57d5439874960146"
+		"https://min-api.cryptocompare.com/data/v2/histoday?fsym=BTC&tsym=USD&limit=119&api_key=0646cc7b8a4d4b54926c74e0b20253b57fd4ee406df79b3d57d5439874960146"
 	);
 	const json = await response.json();
 	const data = json.Data.Data;
