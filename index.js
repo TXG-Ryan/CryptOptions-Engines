240b30add100c8c10e6e660188a754bdaec97f48
TXG-Ryan <ryan.brown@txg.ie>
2020-11-03 08:46:43 +1030

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+
+
+
+
 var options_update_frequency     = 5000;
 var spot_update_frequency        = 3000;
 var stylechange_update_frequency = 3000;
@@ -53,6 +57,7 @@ function initialize_spot()
     setTimeout( setSpot, 1, 'BTC', 'USD' );
     setTimeout( setSpot, 1, 'ETH', 'USD' );
     setTimeout( setSpot, 1, 'BCH', 'USD' );
+    setTimeout( setSpot, 1, 'LTC', 'USD' );
 
 }
 
@@ -71,40 +76,40 @@ function setSpot( asset, ccy )
         async  : true,
         context: document.body
 
-    // }).done(function (jresponse) {
+    }).done(function (jresponse) {
 
-    //     var response = JSON.parse( jresponse );
+        var response = JSON.parse( jresponse );
 
-    //     if( response.data != document.getElementById( asset + '-' + ccy ).value )
-    //     {
+        if( response.data != document.getElementById( asset + '-' + ccy ).value )
+        {
 
-    //         var elem   = document.getElementById( asset + '-' + ccy);
-    //         var nstyle = null;
-    //         var ostyle = elem.style;
-    //         if( response.data > elem.value )
-    //             nstyle = "color: green; font-weight: bold;";
-    //         else
-    //             nstyle = "color: red; font-weight: bold;";
-    //         elem.style = nstyle;
-    //         setTimeout( setStyle, stylechange_update_frequency, elem, ostyle );
+            var elem   = document.getElementById( asset + '-' + ccy);
+            var nstyle = null;
+            var ostyle = elem.style;
+            if( response.data > elem.value )
+                nstyle = "color: green; font-weight: bold;";
+            else
+                nstyle = "color: red; font-weight: bold;";
+            elem.style = nstyle;
+            setTimeout( setStyle, stylechange_update_frequency, elem, ostyle );
 
-    //         var astart     = '<a onclick="alert(\'Go to www.cryptoptions.ie to trade\')">';
-    //         var aend       = '</a>';
-    //         elem.value     = Number(response.data);
-    //         elem.innerHTML =  astart + numberWithCommas( formatNumber( Number(response.data), 2 ) ) + aend;
+            var astart     = '<a onclick="alert(\'Go to www.cryptoptions.ie to trade\')">';
+            var aend       = '</a>';
+            elem.value     = Number(response.data);
+            elem.innerHTML =  astart + numberWithCommas( formatNumber( Number(response.data), 2 ) ) + aend;
 
-    //         var gbpusd = document.getElementById( 'GBPUSD').value;
-    //         var eurusd = document.getElementById( 'EURUSD').value;
+            var gbpusd = document.getElementById( 'GBPUSD').value;
+            var eurusd = document.getElementById( 'EURUSD').value;
 
-    //         document.getElementById( asset + '-GBP' ).value     = Number(response.data)/gbpusd;
-    //         document.getElementById( asset + '-GBP' ).innerHTML = astart + numberWithCommas( formatNumber( Number(response.data)/gbpusd, 2 ) ) + aend;
+            document.getElementById( asset + '-GBP' ).value     = Number(response.data)/gbpusd;
+            document.getElementById( asset + '-GBP' ).innerHTML = astart + numberWithCommas( formatNumber( Number(response.data)/gbpusd, 2 ) ) + aend;
 
-    //         document.getElementById( asset + '-EUR' ).value     = Number(response.data)/eurusd;
-    //         document.getElementById( asset + '-EUR' ).innerHTML = astart + numberWithCommas( formatNumber( Number(response.data)/eurusd, 2 ) ) + aend;
+            document.getElementById( asset + '-EUR' ).value     = Number(response.data)/eurusd;
+            document.getElementById( asset + '-EUR' ).innerHTML = astart + numberWithCommas( formatNumber( Number(response.data)/eurusd, 2 ) ) + aend;
 
-    //     }
+        }
 
-    // });
+    });
 
     setTimeout( setSpot, spot_update_frequency, asset, ccy );
 
@@ -113,7 +118,7 @@ function setSpot( asset, ccy )
 function initialize_options()
 {
 
-    var assets = ['BTC', 'ETH', 'BCH'];
+    var assets = ['BTC', 'ETH', 'BCH', 'LTC'];
     for( var i = 0; i < assets.length; i++ )
     {
 
@@ -128,30 +133,35 @@ function initialize_options()
             async  : true,
             context: document.body
 
-        // }).done(function (jresponse) {
+        }).done(function (jresponse) {
 
-        //     var response = JSON.parse( jresponse );
-        //     for( var x = 0; x < response.data.expiries.length; x++ )
-        //     {
+            var response = JSON.parse( jresponse );
+            for( var x = 0; x < response.data.expiries.length; x++ )
+            {
 
-        //         document.getElementById( response.data.asset + '_EXPIRY_' + (x+1).toString() ).innerHTML = response.data.expiries[x];
-        //         for( var y = 0; y < response.data.strikes.length; y++ )
-        //         {
-        //             document.getElementById( response.data.asset + '_STRIKE_' + (x + 1).toString() + '_' + (y + 1).toString() ).innerHTML = response.data.strikes[y];
-        //             setTimeout( setOptions, 1 , build_id( response.data.asset, response.data.expiries[x], response.data.strikes[y], 'C', 'USD', 'ask_price' ), x, y );
-        //             setTimeout( setOptions, 1 , build_id( response.data.asset, response.data.expiries[x], response.data.strikes[y], 'P', 'USD', 'ask_price' ), x, y );
-        //             setTimeout( setOptions, 1 , build_id( response.data.asset, response.data.expiries[x], response.data.strikes[y], 'C', 'USD', 'delta' ), x, y );
-        //             setTimeout( setOptions, 1 , build_id( response.data.asset, response.data.expiries[x], response.data.strikes[y], 'P', 'USD', 'delta' ), x, y );
+                try {
 
-        //         }
+                    document.getElementById(response.data.asset + '_EXPIRY_' + (x + 1).toString()).innerHTML = response.data.expiries[x];
+                    for(var y = 0; y < response.data.strikes.length; y++) {
 
-        //     }
+                        document.getElementById(response.data.asset + '_STRIKE_' + (x + 1).toString() + '_' + (y + 1).toString()).innerHTML = response.data.strikes[y];
+                        setTimeout(setOptions, 1, build_id(response.data.asset, response.data.expiries[x], response.data.strikes[y], 'C', 'USD', 'ask_price'), x, y);
+                        setTimeout(setOptions, 1, build_id(response.data.asset, response.data.expiries[x], response.data.strikes[y], 'P', 'USD', 'ask_price'), x, y);
+                        setTimeout(setOptions, 1, build_id(response.data.asset, response.data.expiries[x], response.data.strikes[y], 'C', 'USD', 'delta'), x, y);
+                        setTimeout(setOptions, 1, build_id(response.data.asset, response.data.expiries[x], response.data.strikes[y], 'P', 'USD', 'delta'), x, y);
 
-        // });
+                    }
+
+                }
+                catch( err ) {}
+
+            }
+
+        });
 
     }
 
-})
+}
 
 function setOptions( id, x, y )
 {
@@ -188,46 +198,49 @@ function setOptions( id, x, y )
         var ccy          = response.id.split('|')[5];
         var request_type = response.id.split('|')[6];
 
-        if( request_type == 'bid_price' || request_type == 'ask_price' )
-        {
+        try {
 
-            let elem = document.getElementById( asset + '_GRID_' + (x + 1).toString() + '_' + (y + 1).toString() + '_' + pc + '_PV' );
-            if( response.data != elem.value )
-            {
+            if(request_type == 'bid_price' || request_type == 'ask_price') {
 
-                let nstyle = null;
-                let ostyle = elem.style;
-                if( response.data > elem.value )
-                    nstyle = "color: green; font-weight: bold;";
-                else
-                    nstyle = "color: red; font-weight: bold;";
-                elem.style = nstyle;
-                setTimeout( setStyle, stylechange_update_frequency, elem, ostyle );
+                let elem = document.getElementById(asset + '_GRID_' + (x + 1).toString() + '_' + (y + 1).toString() + '_' + pc + '_PV');
+                if(response.data != elem.value) {
 
-                elem.value     = response.data;
-                elem.innerHTML = astart + strong + prefix + estrong + formatNumber(response.data, 2) + ' ' + ccy + aend;
+                    let nstyle = null;
+                    let ostyle = elem.style;
+                    if(response.data > elem.value)
+                        nstyle = "color: green; font-weight: bold;";
+                    else
+                        nstyle = "color: red; font-weight: bold;";
+                    elem.style = nstyle;
+                    setTimeout(setStyle, stylechange_update_frequency, elem, ostyle);
+
+                    elem.value     = response.data;
+                    elem.innerHTML = astart + strong + prefix + estrong + formatNumber(response.data, 2) + ' ' + ccy + aend;
+
+                }
+
+            }
+            else if(request_type == 'delta') {
+
+                let elem = document.getElementById(asset + '_GRID_' + (x + 1).toString() + '_' + (y + 1).toString() + '_' + pc + '_' + request_type);
+                if(response.data != elem.value) {
+
+                    let nstyle = "font-weight: bold;";
+                    let ostyle = elem.style;
+                    elem.style = nstyle;
+                    setTimeout(setStyle, stylechange_update_frequency, elem, ostyle);
+
+                    elem.value     = response.data;
+                    elem.innerHTML = astart + strong + '' + estrong + formatNumber(100 * response.data, 2) + '%' + aend;
+                    {/* removed 'delta:' tag from line 228 ''*/
+                    }
+
+                }
 
             }
 
         }
-        else if( request_type == 'delta' )
-        {
-
-            let elem = document.getElementById( asset + '_GRID_' + (x + 1).toString() + '_' + (y + 1).toString() + '_' + pc + '_' + request_type );
-            if( response.data != elem.value )
-            {
-
-                let nstyle = "font-weight: bold;";
-                let ostyle = elem.style;
-                elem.style = nstyle;
-                setTimeout( setStyle, stylechange_update_frequency, elem, ostyle );
-
-                elem.value     = response.data;
-                elem.innerHTML = astart + strong + '' + estrong + formatNumber(100 * response.data, 2) + '%' + aend;        {/* removed 'delta:' tag from line 228 ''*/}
-
-            }
-
-        }
+        catch( err ) {}
 
     });
 
@@ -235,10 +248,10 @@ function setOptions( id, x, y )
 
 }
 
-function build_id( asset, strike, pc, pricing_ccy, code )
+function build_id( asset, expiry, strike, pc, pricing_ccy, code )
 {
 
-    return 'CO' + '|' + asset + '|' + strike + '|' + pc + '|' + pricing_ccy + '|' + code;
+    return 'CO' + '|' + asset + '|' + expiry + '|' + strike + '|' + pc + '|' + pricing_ccy + '|' + code;
 
 }
 
@@ -298,3 +311,5 @@ function setStyle( elem, style )
     elem.style = style;
 
 }
+
+
